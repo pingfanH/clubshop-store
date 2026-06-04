51258e65ea55a1d0316a74b12e7e479addf86563
pingfanH <pingfanh1145@gmail>
2026-06-05 02:29:12 +0800

    fix: 修复聊天页面消失问题
    - 使用固定高度计算替代绝对定位

--- src/views/chat/Index.vue
+++ src/views/chat/Index.vue
@@ -314,17 +314,15 @@ export default {
 <style lang="less" scoped>
 .chat-wrapper {
   position: relative;
-  height: 100%;
+  width: 100%;
+  height: calc(100vh - 100px);
   overflow: hidden;
 }
 
 .chat-container {
   display: flex;
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
+  width: 100%;
+  height: 100%;
   background: #fff;
   overflow: hidden;
 }
